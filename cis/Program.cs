961f7865417bf4fe5044c5eba8f312f964b4d622
user <user@DESKTOP-UBH533L>
2025-02-07 22:05:40 +0400

    Add project files.

---
+++ cis/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis
+{
+    public class Program
+    {
+        static void Main (string[] args)
+        {
+            General general = new General();
+            User user = new User ();
+            Librarin librarin = new Librarin ();
+            label:
+            int pass = general.login ();
+            if (pass == general.User)
+            {
+                Console.WriteLine("Welcome to your Library!!");
+                for(int i = 0; i < 1;) 
+                { 
+                    Console.WriteLine("1.Borrow a book.\n2.Return a book.\n3.View my books.\n4.EXIT.\n5.Shut down.");
+                    Console.Write("Tell US How can We Help You: ");
+                    int num = int.Parse(Console.ReadLine());
+                    switch (num)
+                    {
+                        case 1:
+                            Console.Write("Enter the name of the book you want: ");
+                            string title = Console.ReadLine();
+                            if (librarin.look (title).Equals ("NOT FOUND.") == true)
+                            {
+                                Console.WriteLine("NOT FOUND.");
+                                general.separate ();
+                                break;
+                            }
+                            else
+                            {
+                                user.ADD (title);
+                                librarin.books.Remove (title);
+                                break; 
+                            }
+                        case 2:
+                            Console.Write ("Enter the name of the book you want: ");
+                            string name = Console.ReadLine();
+                            if (user.look (name).Equals ("NOT FOUND.") == true)
+                            {
+                                Console.WriteLine ($"{name} book is not exist.");
+                                general.separate ();
+                                break;
+                            }
+                            else
+                            {
+                                user.RETURN (name);
+                                librarin.books.Add (name);
+                                break;
+                            }
+                        case 3:
+                            user.VIEW ();
+                            break;
+                        case 4:
+                            Console.WriteLine ("EXITING...");
+                            goto label;
+                        case 5:
+                            Console.WriteLine ("Shutting down...");
+                            return;
+                    }
+                }
+            }
+            else if (pass == general.Lib)
+            {
+                Console.WriteLine ("Welcome to your Library!!");
+                for (int i = 0; i < 1;)
+                {
+                    Console.WriteLine ("1.Add a book.\n2.Rmove a book.\n3.View my books.\n4.EXIT.\n5.Shut down.");
+                    Console.Write ("Tell US How can We Help You: ");
+                    int num = int.Parse (Console.ReadLine ());
+                    switch (num)
+                    {
+                        case 1:
+                            Console.Write ("Enter the name of the book you want: ");
+                            string title = Console.ReadLine ();
+                            if (librarin.look (title).Equals ("NOT FOUND.") == false)
+                            {
+                                Console.WriteLine ("Already Exists.");
+                                general.separate ();
+                                break;
+                            }
+                            else
+                            {
+                                librarin.ADD (title);
+                                break;
+                            }
+                        case 2:
+                            Console.Write ("Enter the name of the book you want: ");
+                            string name = Console.ReadLine ();
+                            if (librarin.look (name).Equals ("NOT FOUND.") == true)
+                            {
+                                Console.WriteLine ("The book you want to delete is not found.");
+                                general.separate ();
+                                break;
+                            }
+                            else
+                            {
+                                librarin.REMOVE (name);
+                                break;
+                            }
+                        case 3:
+                            librarin.VIEW ();
+                            break;
+                        case 4:
+                            Console.WriteLine ("EXITING...");
+                            goto label;
+                        case 5:
+                            Console.WriteLine("Shutting down...");
+                            return;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine ("INVALID COMMAND!");
+                general.separate ();
+            }
+        }
+    }
+}
---
+++ cis/prog.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Linq;
+using System.Net.Mime;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis
+{
+    public class General
+    {
+        int user = 2003;
+        int lib = 1200;
+        public int User 
+        {
+            get { return user; }
+        }
+        public int Lib
+        {
+            get { return lib; }
+        }
+        public int login ()
+        {
+            Console.Write("Please Enter Your PASS Position: ");
+            int pass = int.Parse(Console.ReadLine ());
+            return pass;
+        }
+        public void separate ()
+        {
+            Console.WriteLine("==============================");
+        }
+    }
+    public class User:General
+    {
+        List<string> bbooks = new List<string> ();
+        public void ADD(string x)
+        {
+            bbooks.Add (x);
+            Console.WriteLine($"{x} Book has been borrowed successfully!");
+            separate ();
+        }
+        public string look (string name)
+        {
+            for (int i = 0; i < bbooks.Count; i++)
+            {
+                if (name.Equals (bbooks[i]) == true)
+                {
+                    return bbooks[i];
+                }
+                else if (i == bbooks.Count - 1)
+                {
+                    return "NOT FOUND.";
+                }
+            }
+            return "NOT WORKING.";
+        }
+        public void VIEW ()
+        {
+            Console.WriteLine("Your BOOKS:");
+            for (int i = 0; i < bbooks.Count; i++)
+            {
+                Console.WriteLine ($"{i + 1}.{bbooks[i]}");
+            }
+            separate ();
+        }
+        public void RETURN(string x)
+        {
+            bbooks.Remove (x);
+            Console.WriteLine($"{x} book has been returned successfully!");
+            separate ();
+        }
+    }
+    public class Librarin:General
+    {
+        public List<string> books = new List<string>();
+        public Librarin ()
+        {
+            books.Add ("Point Zero");
+            books.Add ("Subs");
+        }
+        public string look(string name)
+        {
+            for(int i = 0; i < books.Count; i++)
+            {
+                if (name.Equals (books[i]) == true)
+                {
+                    return books[i];
+                }
+                else if(i == books.Count - 1)
+                {
+                    return "NOT FOUND.";
+                }
+            }
+            return "NOT WORKING.";
+        }
+        public void REMOVE(string name)
+        {
+            books.Remove (name);
+            Console.WriteLine ($"{name} book has been deleted successfully!");
+            separate ();
+        }
+        public void ADD(string name)
+        {
+            books.Add (name);
+            Console.WriteLine ($"{name} Book has been added to the library!");
+            separate ();
+        }
+        public void VIEW ()
+        {
+            Console.WriteLine ("Library`s BOOKS:");
+            for (int i = 0; i < books.Count; i++)
+            {
+                Console.WriteLine ($"{i + 1}.{books[i]}");
+            }
+            separate ();
+        }
+    }
+}
